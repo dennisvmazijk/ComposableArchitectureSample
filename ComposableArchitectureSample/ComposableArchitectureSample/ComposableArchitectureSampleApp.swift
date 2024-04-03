2792c7b1ad99b1a84a7688aadf00f6cfd10c0d90
Dennis van Mazijk <144079059+dennisvmazijk@users.noreply.github.com>
2024-04-03 14:43:46 +0900

    Added TCA SDK

---
+++ ComposableArchitectureSample/ComposableArchitectureSample/ComposableArchitectureSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableArchitectureSampleApp.swift
+//  ComposableArchitectureSample
+//
+//  Created by Dennis van Mazijk on 2024/04/03.
+//
+
+import SwiftUI
+
+@main
+struct ComposableArchitectureSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
